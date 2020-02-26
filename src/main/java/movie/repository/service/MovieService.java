dc217b05263187491b17d1bfa32d1950ab9fb7a7
Jorge <jsanzbriones@gmail.com>
2020-02-26 11:16:09 +0100

    first DI

---
+++ src/main/java/movie/repository/service/MovieService.java
@@ -0,0 +1,12 @@
+package movie.repository.service;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class MovieService {
+
+
+    public String get() {
+        return "hello World";
+    }
+}
--- src/main/java/movie/repository/web/MovieController.java
+++ src/main/java/movie/repository/web/MovieController.java
@@ -3,12 +3,19 @@ package movie.repository.web;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import movie.repository.service.MovieService;
 
 @Controller("/movie")
 public class MovieController {
 
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
     @Get(produces = MediaType.TEXT_PLAIN)
-    public String index() {
-        return "Hello World";
+    public String getMovies() {
+        return movieService.get();
     }
 }
